03915a6b9e18ae445aa5d5a59b53e0e4e90013f8
bgg <bkg6638@naver.com>
2025-03-25 23:26:29 +0900

    .

--- P5/sketch.js
+++ P5/sketch.js
@@ -55,7 +55,7 @@ function setup() {
     reaction_timer[i] = 0; 
     reaction_state[i] = 0; 
   }
-  mode_state = -1; // 최초 시작시 모드는 -1 detect_mode로 설정합니다.
+  mode_state = 2; // 최초 시작시 모드는 2 detect_mode로 설정합니다.
 
 
   pen_stroke_num = -1;  // 최초 시작시 획은 없음으로 -1; 
@@ -146,11 +146,11 @@ function finger_detect(){
 }
 
 // 모드를 변경할때 대기하는 시간을 처리합니다.
-function  mode_change(cur_mode,target_state,_r,_g,_b,){
+function  mode_change(target_state,_r,_g,_b,){
   
-  mode_timer[cur_mode]++; //타이머를 증가시킵니다.
+  mode_timer[target_state]++; //타이머를 증가시킵니다.
   
-  if( mode_timer[cur_mode]>120){ //타이머가 120 이상 즉, 1초당 60프레임이므로, 2초가 지나면 실행
+  if( mode_timer[target_state]>120){ //타이머가 120 이상 즉, 1초당 60프레임이므로, 2초가 지나면 실행
     
     //모드 변경을 위한 타이머들을 초기화 시킵니다.
     mode_timer[0] = 0; 
@@ -168,8 +168,8 @@ function  mode_change(cur_mode,target_state,_r,_g,_b,){
 
   }
 
-  fill(_r,_g,_b,mode_timer[cur_mode]) // 투명도로 타이머의 진행정도를 나타냄
-  circle(640 - hands[0].keypoints[8].x,hands[0].keypoints[8].y,mode_timer[cur_mode]/2 ); //원의 크기로 타이머의 진행 정도를 타나냄
+  fill(_r,_g,_b,mode_timer[target_state]) // 투명도로 타이머의 진행정도를 나타냄
+  circle(640 - hands[0].keypoints[8].x,hands[0].keypoints[8].y,mode_timer[target_state]/2 ); //원의 크기로 타이머의 진행 정도를 타나냄
   noFill()
   stroke(4,150);
   circle(640 - hands[0].keypoints[8].x,hands[0].keypoints[8].y,60); //타이머의 최종 도달 범위를 나타냄
@@ -177,7 +177,7 @@ function  mode_change(cur_mode,target_state,_r,_g,_b,){
 }
 
 
-
+//그림을 그려질 좌표를 저장하는 함수입니다.
 function draw_pencil(){
 
   if( draw_state == 1 || draw_state %30 == 0 ){ // draw_state가 변경되거나,30frame마다 획을 구분함
@@ -190,128 +190,150 @@ function draw_pencil(){
   pencil[pen_stroke_num].push(  {x:640-hands[0].keypoints[8].x , y:hands[0].keypoints[8].y} )  // show_draw에서 그려질 라인의 좌표를 저장합니다.
   
 }
+//그려진 그림을 삭제하는 함수입니다.
 function erase_pencil(){
   push();
   fill(0,0,0,50);
   circle(640 - hands[0].keypoints[12].x, hands[0].keypoints[12].y,30); //지우개의 영역을 표시합니다. 
   pop();
 
-  for(let i = 0; i<pencil.length ; i++){
-    for(let j = 0; j<pencil[i].length; j++){
+  for(let i = 0; i<pencil.length ; i++){ // 그려진 그림의 획 만큼 반복합니다.
+    for(let j = 0; j<pencil[i].length; j++){ // 각 획에 저장된 좌표 갯수만큼 반복합니다.
 
 
       if(  ( pencil[i][j].x -(640-hands[0].keypoints[12].x) )**2  + ( pencil[i][j].y -(hands[0].keypoints[12].y) )**2 < 225    ){
+        //현재 중지손가락 끝과 그림그려진 좌표사이의 거리가 15보다 작은경우
+        //현재 좌표가 저장된 획을 전부 지웁니다.
         pencil[i] = [];
       }
       
     }
   }
-  draw_state  = 1;
+  draw_state  = 1; //그림을 그릴 경우 새로운 획을 시작하기 위해 1로 설정합니다. 
 
 }
 
 
 
-
+/////펴져있는 손가락 갯수를 통해서 반응모드,그림모드로 전환합니다.
 function mode_detect(){
   push()
   
-  finger_detect();
+  finger_detect(); //손가락 갯수를 계산합니다.
   
-  if(cur_pinger_num == 0){
-    mode_change(0,0,0,0,255)
+  if(cur_pinger_num == 0){ //주먹을 쥔 상태인경우 
+    mode_change(0,0,0,255)  // 0번째 모드(반응 모드)로 120fram이후 변경합니다. 대기시간을 나타내는 원을 fill(0,0,255)로 나타냅니다. 
    
   }
   else if(cur_pinger_num == 1 && cur_pinger_state[1] == 1 ){
 
-    mode_change(1,1,255,255,0)
+    mode_change(1,255,255,0) // 1번째 모드(그림모드)로 120fram이후 변경합니다. 대기시간을 나타내는 원을 fill(255,255,0)로 나타냅니다.
     
   }
   pop()
 }
+
+///// 검지손가락을 통해서 원하는 이미지를 선택합니다.
 function reaction_mode(){
   push();
   fill(0,0,255,50);
-  circle(640 - hands[0].keypoints[8].x,hands[0].keypoints[8].y,5);
+  circle(640 - hands[0].keypoints[8].x,hands[0].keypoints[8].y,5); // 현재 검지의 좌표를 나타냅니다.
   pop();
   
-  for(let i =0; i<4; i++){
-    image(reaction_image[i],reaction_menu_x -70 + i*50 ,reaction_menu_y,40,40);
-    if(  (reaction_menu_x -50 + i*50  - (640 - hands[0].keypoints[8].x))**2 + ( reaction_menu_y  -  hands[0].keypoints[8].y)**2    < 1600      ){
-      reaction_state[i]++;
+  for(let i =0; i<4; i++){ // 4개의 이미지가 설정되었음으로 이미지의 수 만큼 반복합니다.
 
-      if( reaction_state[i] > 60){
-        mode_state = -1;
-        reaction_state[0] = 0;
+    //각 40*40의 크기의 이미지를를 50의 간격으로 보여줍니다.
+    image(reaction_image[i],reaction_menu_x -70 + i*50 ,reaction_menu_y,40,40);
+    
+    if(  (reaction_menu_x -50 + i*50  - (640 - hands[0].keypoints[8].x))**2 + ( reaction_menu_y  -  hands[0].keypoints[8].y)**2    < 1600      ){
+      //각 이미지와 검지의 좌표의 거리차가 40이내인경우
+      reaction_state[i]++; // 해당하는 이미지의 상태값을 증가시킵니다.
+
+      if( reaction_state[i] > 60){ // 이미지가 60프레임이상 선택되어있어다면
+
+        mode_state = 2; // 다시 기본모드(mode_detect)로 설정합니다.
+        
+        // 이후 다시 반응 모드로 진입했을때를 위해 초기화 합니다.
+        reaction_state[0] = 0; 
         reaction_state[1] = 0;
         reaction_state[2] = 0;
         reaction_state[3] = 0;
-        reaction_timer[i] = 180
+
+        reaction_timer[i] = 180 // 해당 이미지가 화면에 떠 있는 시간을 180frame으로 설정합니다.
       }
     }
   }
 
 
 }
+
+/////제스처를 통해 그림을 그리거나,지웁니다.
 function draw_mode(){
   
-  finger_detect();
+  finger_detect(); //손가락을 인식합니다.
   
 
-  if( cur_pinger_num == 2 && cur_pinger_state[1] == 1 && cur_pinger_state[2] == 1 ){ //그리기
+  if( cur_pinger_num == 2 && cur_pinger_state[1] == 1 && cur_pinger_state[2] == 1 ){
+    //두 개의 손가락이 펴져있고, 검지와 중지인 경우
     
-    draw_pencil();
+    draw_pencil(); //그림을 그리는 함수를 호출합니다.
 
   }
   else if( cur_pinger_num == 3 && cur_pinger_state[1] == 1 && cur_pinger_state[2] == 1 && cur_pinger_state[3] == 1  ){ //지우개
-    
+    // 세 개의 손가락이 펴져있고 검지,중지,약지인경우
     erase_pencil();
   }
-  else if(cur_pinger_num == 0){ //초기화
-    
-    pencil = [];
-    pen_stroke_num = -1;
-    draw_state  = 1;
+  else if(cur_pinger_num == 0){ 
+    //주먹을 쥔 경우
+
+    pencil = []; // 저장된 좌표값들을 모두 초기화 시킵니다.
+    pen_stroke_num = -1; // 획의 인덱스를 초기화 시킵니다.
+    draw_state  = 1; //이후  그림을 그릴 때 새로운 획부터 시작하도록 합니다.
   }
   else if( cur_pinger_num == 5 ){ // 모드선택으로 돌아가기 
  
-    mode_change(1,-1,255,255,255)
+    mode_change(2,255,255,255) // 4 (= 기본 모드 = mode_detect )로 변경합니다. 대기시간을 나타내는 원을 fill(255,255,255)로 설정합니다.
 
+    draw_state  = 1; //이후  그림을 그릴 때 새로운 획부터 시작하도록 합니다.
   }
-  else{
+  else{ //아무런 제스쳐도 아닌 경우
     push();
-    fill(255,150);
-    circle(640-hands[0].keypoints[8].x,hands[0].keypoints[8].y,5);
+    fill(255,150); 
+    circle(640-hands[0].keypoints[8].x,hands[0].keypoints[8].y,5);// 그림이 그려지는 검지의 좌표를 표시해줍니다.
     pop();
-    draw_state  = 1;
+    draw_state  = 1; //이후  그림을 그릴 때 새로운 획부터 시작하도록 합니다.
   }
   
 
 }
 
 
-
+//draw에서 지속적으로 호출되며 pencil변수에 저장된 좌표로 라인을 그려줍니다.
 function show_draw(){
   push()
-  stroke(255,255,0);
-  strokeWeight(3);
-  fill(255);
-  for(let i = 0; i<pencil.length ; i++){
-    for(let j = 1; j<pencil[i].length; j++){
-      line( pencil[i][j-1].x, pencil[i][j-1].y,  pencil[i][j].x,  pencil[i][j].y  );
+
+  stroke(255,255,0); //라인을 노란색으로 지정합니다.
+  strokeWeight(3); // 라인의 굵기를 3으로 지정합니다.
+  
+  for(let i = 0; i<pencil.length ; i++){ // 획의 갯수 만큼 반복합니다.
+    for(let j = 1; j<pencil[i].length; j++){ // 현재 획의 저장된 좌표의 갯수-1만큼 저장합니다.
+      line( pencil[i][j-1].x, pencil[i][j-1].y,  pencil[i][j].x,  pencil[i][j].y  );//저장된 좌표를 그립니다.
     }
   }
+
   pop();
 }
+
+//draw에서 지속적으로 호출되며 reaction_timer변수에 저장된 frame의 수만큼 이미지를 보여줍니다.
 function show_reaction(){
 
   push();
 
-  for(let i = 0; i<4; i++){
-    if(reaction_timer[i]>0){
-      tint(255,reaction_timer[i]+40);
-      image(reaction_image[i],reaction_menu_x,reaction_menu_y,120,120);
-      reaction_timer[i]--;
+  for(let i = 0; i<4; i++){ // 4개의 이미지에 대해서
+    if(reaction_timer[i]>0){ // reaction_timer변수에 시간이 있다면
+      tint(255,reaction_timer[i]+40); // 투명도를 현재 남은 시간만큼으로 설정하여 점점사라지는 효과를 줍니다.
+      image(reaction_image[i],reaction_menu_x,reaction_menu_y,120,120);//120*120의 이미지를 보여줍니다.
+      reaction_timer[i]--; //   reaction_timer가 0이 될때까지 반복할 수 있도록 1 감소시킵니다.
     }
   }
   pop();
